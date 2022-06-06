278d18220773f941d2d431d906353f48577e6814
Max Petretta <git@maxpetretta.com>
2022-06-05 20:46:22 -0400

    Enable prettier for jsx files

--- react/components/AppProvider.jsx
+++ react/components/AppProvider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, createContext } from "react"
+import { useState, useContext, createContext } from "react"
 import { useContractRead, useContractEvent } from "wagmi"
 import { contractAddress, contractABI } from "../lib/contract.js"
 
