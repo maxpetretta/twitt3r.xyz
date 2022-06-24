1ae73395b6262cc8398a79bf5ab67bcd883f1131
Max Petretta <git@maxpetretta.com>
2022-06-24 16:40:09 -0400

    Switched from require() statements to thrown errors

--- hardhat/contracts/Twitt3r.sol
+++ hardhat/contracts/Twitt3r.sol
@@ -27,17 +27,83 @@ contract Twitt3r is Ownable, Pausable {
     uint256 retweetID;
   }
 
-  // Mapped struct with index, see: https://ethereum.stackexchange.com/a/13168
   uint256 public id = 1; // Use id 0 for top-level tweets
   uint256[] public tweetIDs;
-  mapping(uint256 => Tweet) public tweets;
-
+  mapping(uint256 => Tweet) public tweets; // Mapped struct with index, see: https://ethereum.stackexchange.com/a/13168
   mapping(address => uint256) public lastTweetedAt;
+  
+  /**
+   * @notice Emitted when a new tweet is added to the contract
+   * @param id The new tweet's ID
+   * @param from The sender's address
+   * @param timestamp The block timestamp of the new tweet
+   * @param message The message from the tweet's sender
+   * @param deleted Flag for marking the tweet as deleted
+   * @param replyID The ID of the tweet being replied to, 0 for top-level tweets
+   * @param retweetID The ID of the tweet being resent, 0 for top-level tweets
+   */
   event NewTweet(uint256 indexed id, address indexed from, uint256 timestamp, string message, bool deleted, uint256 replyID, uint256 retweetID);
+
+  /**
+   * @notice Emitted when an existing tweet is modified by it's author
+   * @param id The existing tweet's ID
+   * @param from The sender's address
+   * @param timestamp The block timestamp of the existing tweet
+   * @param message The edited message from the tweet's sender
+   * @param deleted Flag for marking the tweet as deleted
+   * @param replyID The ID of the tweet being replied to, 0 for top-level tweets
+   * @param retweetID The ID of the tweet being resent, 0 for top-level tweets
+   */
   event EditTweet(uint256 indexed id, address indexed from, uint256 timestamp, string message, bool deleted, uint256 replyID, uint256 retweetID);
+
+  /**
+   * @notice Emitted when an existing tweet is deleted from the contract
+   * @param id The existing tweet's ID
+   * @param from The sender's address
+   * @param timestamp The block timestamp of the new tweet
+   * @param message The message from the tweet's sender
+   * @param deleted Flag for marking the tweet as deleted
+   * @param replyID The ID of the tweet being replied to, 0 for top-level tweets
+   * @param retweetID The ID of the tweet being resent, 0 for top-level tweets
+   */
   event DeleteTweet(uint256 indexed id, address indexed from, uint256 timestamp, string message, bool deleted, uint256 replyID, uint256 retweetID);
+
+  /**
+   * @notice Emitted when the contract owner clears all tweets from the contract
+   * @param id The ID number of the last valid tweet
+   */
   event ClearTweets(uint256 id);
+
+  /**
+   * @notice Emitted when a sender has won the contract lottery
+   * @param winner The winner's wallet address
+   * @param jackpot The value of the winner's jackpot
+   */
   event WonLottery(address winner, uint256 jackpot);
+
+  /// @notice Thrown when the sender is not authorized to perform an action
+  error Unauthorized();
+
+  /// @notice Thrown when the given tweet ID does not exist
+  error InvalidID();
+
+  /// @notice Thrown when the contract is sent the incorrect amount of ether
+  error InvalidPrice();
+
+  /// @notice Thrown when the sent message is over the allowed character length
+  error InvalidMessage();
+
+  /// @notice Thrown when the given tweet has been deleted
+  error DeletedTweet();
+
+  /// @notice Thrown when the sender is still on a tweet cooldown period
+  error SenderCooldown();
+
+  /// @notice Thrown when a contract withdrawal fails
+  error WithdrawalFailed();
+
+  /// @notice Thrown when the contract balance can not payout a jackpot win
+  error InsufficientBalance();
 
   /**
    * @notice Deploys the Twitt3r contract with the given settings
@@ -51,18 +117,14 @@ contract Twitt3r is Ownable, Pausable {
     jackpot = _jackpot;
   }
 
-  /**
-   * @notice Deposit into the contract to refill for jackpot payouts
-   */
+  /// @notice Enable contract deposit to refill for jackpot payouts
   function deposit() public payable {}
 
-  /**
-   * @notice In case of emergency, the owner can withdraw from the contract
-   */
+  /// @notice In case of emergency, the owner can withdraw from the contract
   function withdraw() public onlyOwner {
     uint256 amount = address(this).balance;
     (bool sent, ) = owner().call{value: amount}("");
-    require(sent, "Failed to withdraw from contract");
+    if (!sent) revert WithdrawalFailed();
   }
 
   /**
@@ -72,12 +134,12 @@ contract Twitt3r is Ownable, Pausable {
    * @param _retweetID The ID of the tweet being resent, 0 for top-level tweets
    */
   function newTweet(string memory _message, uint256 _replyID, uint256 _retweetID) public payable whenNotPaused {
-    require(msg.value >= price, "Amount sent is incorrect");
-    require(bytes(_message).length <= 280, "Limit is 280 characters!");
-    require(lastTweetedAt[msg.sender] + 1 minutes < block.timestamp, "Please wait a minute before tweeting again!");
-    lastTweetedAt[msg.sender] = block.timestamp;
+    if (msg.value < price) revert InvalidPrice();
+    if (bytes(_message).length > 280) revert InvalidMessage();
+    if (lastTweetedAt[msg.sender] + 1 minutes >= block.timestamp) revert SenderCooldown();
 
-    console.log("%s has tweeted!", msg.sender); // DEBUG
+    // console.log("%s has tweeted!", msg.sender); // DEBUG
+    lastTweetedAt[msg.sender] = block.timestamp;
     tweets[id] = Tweet(msg.sender, block.timestamp, _message, false, _replyID, _retweetID);
     tweetIDs.push(id);
 
@@ -95,12 +157,12 @@ contract Twitt3r is Ownable, Pausable {
    * @param _message The new replacement message for the specified tweet
    */
   function editTweet(uint256 _id, string memory _message) public whenNotPaused {
-    require(tweets[_id].timestamp != 0, "Given ID is invalid");
-    require(!tweets[_id].deleted, "Given tweet is deleted!");
-    require(tweets[_id].from == msg.sender, "Sender does not match the given tweet");
-    require(bytes(_message).length <= 280, "Limit is 280 characters!");
+    if (tweets[_id].timestamp == 0) revert InvalidID();
+    if (tweets[_id].deleted) revert DeletedTweet();
+    if (tweets[_id].from != msg.sender) revert Unauthorized();
+    if (bytes(_message).length > 280) revert InvalidMessage();
+    
     tweets[_id].message = _message;
-
     emit EditTweet(_id, msg.sender, block.timestamp, tweets[_id].message, tweets[_id].deleted, tweets[_id].replyID, tweets[_id].retweetID);
   }
 
@@ -109,17 +171,15 @@ contract Twitt3r is Ownable, Pausable {
    * @param _id The ID of the tweet to delete
    */
   function deleteTweet(uint256 _id) public whenNotPaused {
-    require(tweets[_id].timestamp != 0, "Given ID is invalid");
-    require(!tweets[_id].deleted, "Given tweet is already deleted!");
-    require(tweets[_id].from == msg.sender, "Sender does not match the given tweet");
+    if (tweets[_id].timestamp == 0) revert InvalidID();
+    if (tweets[_id].deleted) revert DeletedTweet();
+    if (tweets[_id].from != msg.sender) revert Unauthorized();
+    
     tweets[_id].deleted = true;
-
     emit DeleteTweet(_id, msg.sender, block.timestamp, tweets[_id].message, tweets[_id].deleted, tweets[_id].replyID, tweets[_id].retweetID);
   }
 
-  /**
-   * @notice In case of emergency, the owner can clear all tweets from the contract
-   */
+  /// @notice In case of emergency, the owner can clear all tweets from the contract
   function clear() public onlyOwner {
     uint256 lastID = id;
     for (uint256 i = 0; i < tweetIDs.length; i++) {
@@ -131,16 +191,12 @@ contract Twitt3r is Ownable, Pausable {
     emit ClearTweets(lastID);
   }
 
-  /**
-   * @notice Allow the owner to pause tweeting functions
-   */
+  /// @notice Allow the owner to pause tweeting functions
   function pause() public onlyOwner {
     _pause();
   }
 
-  /**
-   * @notice Allow the owner to unpause tweeting functions
-   */
+  /// @notice Allow the owner to unpause tweeting functions
   function unpause() public onlyOwner {
     _unpause();
   }
@@ -263,7 +319,8 @@ contract Twitt3r is Ownable, Pausable {
    * @param _sender The most recent tweet sender
    */
   function checkLottery(address payable _sender) private {
-    require(address(this).balance >= jackpot, "Contract balance is insufficient!");
+    if (address(this).balance < jackpot) revert InsufficientBalance();
+
     uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
     console.log("Random # generated: %s", randomNumber); // DEBUG
     seed = randomNumber;
@@ -272,7 +329,7 @@ contract Twitt3r is Ownable, Pausable {
       console.log("%s has won!", msg.sender); // DEBUG
 
       (bool sent, ) = _sender.call{value: jackpot}("");
-      require(sent, "Failed to withdraw from contract");
+      if (!sent) revert WithdrawalFailed();
       emit WonLottery(_sender, jackpot);
     }
   }
