c81941a0f7d4cbb3133ad39f1e8a9684c66b213c
Max Petretta <git@maxpetretta.com>
2022-07-18 22:28:17 -0400

    Update contract address

--- react/lib/contract.js
+++ react/lib/contract.js
@@ -1,4 +1,4 @@
 import Twitt3r from "./abi/Twitt3r.json"
 
-export const contractAddress = "0x1CA3A909c85E072E180b6e961Ad9c0627f35fb07"
+export const contractAddress = "0x3493B7ABE5e6E142D632e6596bc550A73c87Ee79"
 export const contractABI = Twitt3r.abi
